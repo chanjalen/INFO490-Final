7c0d188c4274f832d5dfbe4f40c9827e12d01188
jalenchan1 <jalen.chan@gmail.com>
2026-05-05 15:55:22 -0500

    url change

--- search/models.py
+++ search/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Movie(models.Model):
@@ -21,6 +22,9 @@ class Movie(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.release_year})"
+
+    def get_absolute_url(self):
+        return reverse("movie_detail", kwargs={"pk": self.pk})
 
     @property
     def genre_list(self) -> list[str]:
